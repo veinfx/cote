8ee02ecc435a65ee73507aad59a520af61f44de7
Park Sung Woo (kevin) <55982173+veinfx@users.noreply.github.com>
2025-09-17 14:10:01 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -1,12 +1,9 @@
 def solution(n):
     answer  = 0
     if n%2:
-        for i in range(1, n+1):
-            if i%2:
-                print(i)
-                answer += i
+        for i in range(1, n+1, 2):
+            answer += i
     else:
-        for i in range(1, n+1):
-            if not i%2:
-                answer += i**2
+        for i in range(2, n+1, 2):
+            answer += i**2
     return answer
