81c547be98e22eb4aa239c3e4044439cd5ea0284
Park Sung Woo (kevin) <55982173+veinfx@users.noreply.github.com>
2025-09-19 18:51:34 +0900

    [level 0] Title: 코드 처리하기, Time: 19.51 ms, Memory: 9.44 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -1,16 +1,3 @@
 def solution(code):
     answer = ''
-    mode = 0
-    for i in range(len(code)):
-        if code[i] == "1" and mode == 0:
-            mode = 1
-        elif code[i] == "1" and mode == 1:
-            mode = 0
-
-        if mode == 0:
-            if code[i] != "1" and i%2==0:
-                answer += code[i]
-        else:
-            if code[i] != "1" and i%2==1:
-                answer += code[i]
-    return answer if answer else "EMPTY"
+    return answer
