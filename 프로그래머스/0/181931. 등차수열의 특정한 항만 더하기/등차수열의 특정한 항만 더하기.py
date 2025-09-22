85249e375ea5edef9357589ed0cf4cd9792cc5ad
Park Sung Woo (kevin) <55982173+veinfx@users.noreply.github.com>
2025-09-22 15:33:39 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.03 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -1,3 +1,8 @@
+# 등차수열 n번째 항은 a+(n−1)da+(n−1)d 이지만 반복문 인덱스가 0부터 시작하므로 a+n×da+n×d 로 계산.
 def solution(a, d, included):
     answer = 0
+    for n in range(len(included)):
+        y = a + n * d
+        if included[n]:
+            answer += y
     return answer
