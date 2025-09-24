9e1a4d56d6144b97160ed7f6c89daedb4e1a7158
Park Sung Woo (kevin) <55982173+veinfx@users.noreply.github.com>
2025-09-24 11:10:05 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,13 +1,7 @@
 def solution(num_list):
-    answer = 0
-    x = 1
-    y = 0
-    for i in range(len(num_list)):
-        x = x * num_list[i]
-        y = y + num_list[i]
-        i+=1
-    if x < y**2:
-        answer = 1
-    elif x > y**2:
-        answer = 0
-    return answer
+    multy = 1
+    for i in num_list:
+        multy = multy * i
+    plus = sum(num_list)
+    plus = plus*plus
+    return 1 if plus > multy else 0
