edd66fb6ffb4662e80dd273d2eed264fb1fc15d5
Park Sung Woo (kevin) <55982173+veinfx@users.noreply.github.com>
2025-09-28 00:35:07 +0900

    [level 0] Title: 수 조작하기 1, Time: 4.95 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,11 +1,9 @@
 def solution(n, control):
+    c = {'w': 1,
+         's': -1,
+         'd': 10,
+         'a': -10
+    }
     for i in control:
-        if i == "w":
-            n += 1
-        elif i == "s":
-            n -= 1
-        elif i == "d":
-            n += 10
-        elif i == "a":
-            n -= 10
+        n += c[i]
     return n
